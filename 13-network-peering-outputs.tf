c1db750336267b29dd7665d15f38061378b4d298
Kara Nottingham <45759232+karanotts@users.noreply.github.com>
2020-12-18 14:32:40 +0000

    use the equals sign to assign it a value

--- 13-network-peering-outputs.tf
+++ 13-network-peering-outputs.tf
@@ -1,5 +1,5 @@
 data "null_data_source" "network_peering_outputs" {
-  inputs {
+  inputs = {
     network_peering_requested_id = "${azurerm_virtual_network_peering.virtual_network_peering_requester.id}"
     network_peering_accepeter_id = "${azurerm_virtual_network_peering.virtual_network_peering_accepter.id}"
   }
